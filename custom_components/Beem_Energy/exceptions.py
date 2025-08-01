bb4f81e9918bf15647cda12669143c1f34442949
Charles P <charles.picaud39@gmail.com>
2025-08-01 23:27:09 +0200

    Create exceptions.py

---
+++ custom_components/Beem_Energy/exceptions.py
@@ -0,0 +1,7 @@
+class BeemAuthError(Exception):
+    """Erreur d'authentification Beem."""
+    pass
+
+class BeemConnectionError(Exception):
+    """Erreur de connexion au serveur Beem."""
+    pass
